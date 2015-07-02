61c94d29c566207fa9c3539ce8d78e9d30a3a8c9
Harshit Gupta <harshit@hike.in>
2015-07-02 23:22:28 +0530

    initial commit

---
+++ CG/readme.txt
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+Group members:
+	Snehil Shwetabh	2011C6PS502P
+	Akhil Tripathi	2011C6PS739P
+	Harshit Gupta	2011C6PS837P
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+===============================================================================================
+	APPLICATION : Geometric Transformation Technique for Total Hip Implant
+===============================================================================================
+
+This file contains a summary of what you will find in each of the files that
+make up our application.
+
+System Requirements:
+	-To run the jar set path to the jdk/bin as environment variable,i.e. javaw.exe should be visible to the computer
+	-The images back.jpg and hip.jpg should be in the same folder as the CompGraphProject.jar file
+
+CompGraphProject.jar:
+	-This is the main application the user needs to launch after downloading it.
+	-The digital hip implant with the X- ray image in the background will be shown. 
+	-The user can translate the implant to the required place with the mouse.
+	-The size of the implant can be scaled as per the requirement by providing 
+	 appropiate scaling factor in the textfield.
+	-After scaling, the hip implant can be adjusted in the right place by providing
+	 the angle in degrees.
+	-Mirror button will be used to reflect the implant about y axis.
+	-Scaling and Rotation are relative to their previous values.
+
+CompGraphProject.java:
+	-This is the application source code file.
+	-Contains the code to run the application.
+
+Programming Language Used
+	-Java
+
+Packages Used
+	-javax.swing.*
+		This library was used for making the GUI.
+	-java.awt.*
+		This library was used for graphics.
+
---
+++ src/Home.java
@@ -0,0 +1,279 @@
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+
+public class Home extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void maign(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home frame = new Home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private BufferedImage convetIcon(Icon icon)
+	{
+		BufferedImage bi = new BufferedImage(
+                icon.getIconWidth(),
+                icon.getIconHeight(),
+                BufferedImage.TRANSLUCENT);
+            Graphics g = bi.createGraphics();
+            // paint the Icon to the BufferedImage.
+            icon.paintIcon(null, g, 0,0);
+            
+           
+            g.dispose();
+            
+		return bi;
+		
+	}
+	private  BufferedImage Rotate(BufferedImage originalImage,Double angle) {
+		BufferedImage newImage = null;
+		AffineTransform tx = new AffineTransform();
+
+		// last, width = height and height = width :)
+		tx.translate(originalImage.getHeight()/2 ,originalImage.getWidth()/2);
+
+		tx.rotate(angle);
+		
+		// first - center image at the origin so rotate works OK
+		tx.translate(-originalImage.getWidth()/2 ,-originalImage.getHeight() /2);
+
+		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+
+		// new destination image where height = width and width = height.
+		newImage =new BufferedImage(originalImage.getHeight(), originalImage.getWidth(), originalImage.getType());
+		op.filter(originalImage, newImage);
+		return newImage;
+		}
+
+	private  BufferedImage Reflect(BufferedImage originalImage) {
+		BufferedImage newImage = null;
+		
+		AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+		tx.translate(-originalImage.getWidth(null), 0);
+		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+
+		// new destination image where height = width and width = height.
+		newImage =new BufferedImage(originalImage.getHeight(), originalImage.getWidth(), originalImage.getType());
+		op.filter(originalImage, newImage);
+		return newImage;
+		}
+	
+	private  BufferedImage Scale(BufferedImage originalImage,Double amount) {
+		BufferedImage newImage = null;
+		
+	    if(originalImage != null)
+	    {
+	    	if(amount>1 && amount*originalImage.getWidth()<500 && amount*originalImage.getWidth()!=0) 
+	        {
+	    		newImage = new BufferedImage(amount.intValue()*originalImage.getWidth(), amount.intValue()*originalImage.getHeight(), originalImage.getType());
+	    		}
+	    	else
+	    	{
+	    		newImage = new BufferedImage(originalImage.getWidth(),originalImage.getHeight(), originalImage.getType());
+	    	}
+	        Graphics2D g = newImage.createGraphics();
+	        AffineTransform at =AffineTransform.getScaleInstance(amount,amount);
+			
+	        g.drawRenderedImage(originalImage, at);
+	    }
+	    return newImage;
+		}
+	/**
+	 * Create the frame.
+	 */
+	int click=0,x,y;
+	private JTextField ra;
+	private JTextField sf;
+	public Home() {
+		setTitle("Transplant Graphics");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 520, 520);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		//setResizable(false);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		contentPane.add(layeredPane, BorderLayout.CENTER);
+		
+		final JLabel label = new JLabel("");
+		label.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				
+			}
+		});
+		layeredPane.setLayer(label, 2);
+		label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				click=e.getButton();
+				x=e.getX();
+				y=e.getY();
+				
+			}
+				
+		});
+		
+		
+		label.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if(click==MouseEvent.BUTTON1){
+					Point curr=MouseInfo.getPointerInfo().getLocation();
+					if((curr.x>getX()) && (curr.x<(getX()+getWidth())) && (curr.y>getY()) && (curr.y<(getX()+getHeight())) )
+						label.setLocation(curr.x-getX()-90,curr.y-getY()-90);
+					
+					validate();
+					}
+				
+				
+			}
+		});
+		label.setIcon(new ImageIcon("Hip.png"));
+		label.setBounds(10, 11, 150, 150);
+		layeredPane.add(label);
+		
+		final JLabel bckg = new JLabel("");
+		bckg.setIcon(new ImageIcon("back.jpg"));
+		bckg.setBounds(0, 0, 500, 500);
+		layeredPane.add(bckg);
+		
+		JLabel label_2 = new JLabel("Rotate By:");
+		layeredPane.setLayer(label_2, 1);
+		label_2.setForeground(new Color(204, 255, 102));
+		label_2.setBounds(10, 421, 66, 20);
+		layeredPane.add(label_2);
+		
+		ra = new JTextField();
+		ra.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					Double angle=0.0;
+					try{
+						angle=Math.toRadians(Double.parseDouble(ra.getText()));
+					}
+					catch(Exception se){
+						JOptionPane.showMessageDialog(null, "Invalid degrees!");
+					}
+					ImageIcon rot=new ImageIcon(Rotate(convetIcon(label.getIcon()),angle));
+					label.setSize(rot.getIconWidth(),rot.getIconHeight());
+					label.setIcon(rot);
+					validate();
+				}
+			}
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					bckg.requestFocusInWindow();
+				}
+			}
+		});
+		
+		layeredPane.setLayer(ra, 1);
+		ra.setBounds(10, 440, 86, 20);
+		layeredPane.add(ra);
+		ra.setColumns(10);
+		
+		JLabel lblScaleFactor = new JLabel("Scale Factor:");
+		layeredPane.setLayer(lblScaleFactor, 1);
+		lblScaleFactor.setForeground(new Color(204, 255, 102));
+		lblScaleFactor.setBounds(10, 381, 86, 20);
+		layeredPane.add(lblScaleFactor);
+		
+		sf = new JTextField();
+		layeredPane.setLayer(sf, 1);
+		sf.setColumns(10);
+		sf.setBounds(10, 400, 86, 20);
+		layeredPane.add(sf);
+		
+		JButton btnMirror = new JButton("Mirror");
+		btnMirror.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ImageIcon scal=new ImageIcon(Reflect(convetIcon(label.getIcon())));
+				label.setSize(scal.getIconWidth(),scal.getIconHeight());
+				label.setIcon(scal);
+				validate();
+			}
+		});
+		layeredPane.setLayer(btnMirror, 1);
+		btnMirror.setBounds(399, 11, 89, 23);
+		layeredPane.add(btnMirror);
+		
+		sf.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					Double amount=0.0;
+					try{
+						amount=Double.parseDouble(sf.getText());
+					}
+					catch(Exception se){
+						JOptionPane.showMessageDialog(null, "Invalid Factor!");
+					}
+					ImageIcon scal=new ImageIcon(Scale(convetIcon(label.getIcon()),amount));
+					label.setSize(scal.getIconWidth(),scal.getIconHeight());
+					label.setIcon(scal);
+					validate();
+				}
+			}
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					bckg.requestFocusInWindow();
+				}
+			}
+		});
+	}
+}
